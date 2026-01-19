ccaa22171074dbd128092aae57f366c69bf06737
GeoASD <132585381+GeoASD@users.noreply.github.com>
2026-01-19 11:57:08 -0500

    Refactor popup function to use lowercase properties

--- js/popup.js
+++ js/popup.js
@@ -2,20 +2,21 @@
 function popup_proyectos(feature, layer){
 	if(feature.properties && feature.properties.Proyecto){
 		var popupcontent =
-							"<br>"+"<b>Proyecto: </b>" + feature.properties.Proyecto +
-							"<br>"+"<b>Código: </b>" + feature.properties.Código +
-							"<br>"+"<b>Sector: </b>" + feature.properties.Sector +
-							"<br>"+"<b>Tipo de servicio: </b>" + feature.properties.Tipo_de_Se +
-							"<br>"+"<b>Departamento: </b>" + feature.properties.Departamen +
-							"<br>"+"<b>Provincia: </b>" + feature.properties.Provincia +
-							"<br>"+"<b>Distrito: </b>" + feature.properties.Distrito +
-							"<br>"+"<b>Información disponible: </b>"+ feature.properties.Informaci;
+							"<br>"+"<b>Proyecto: </b>" + feature.properties.proyecto +
+							"<br>"+"<b>Código: </b>" + feature.properties.código +
+							"<br>"+"<b>Sector: </b>" + feature.properties.sector +
+							"<br>"+"<b>Tipo de servicio: </b>" + feature.properties.tipo_de_se +
+							"<br>"+"<b>Departamento: </b>" + feature.properties.departamen +
+							"<br>"+"<b>Provincia: </b>" + feature.properties.provincia +
+							"<br>"+"<b>Distrito: </b>" + feature.properties.distrito +
+							"<br>"+"<b>Información disponible: </b>"+ feature.properties.informaci;
 							
 		layer.bindPopup(popupcontent,{
 			className: "popup"
 		});
 	}
 };
+
 
 
 
